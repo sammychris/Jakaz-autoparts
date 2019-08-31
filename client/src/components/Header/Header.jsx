2a5fd697d1e4a23cf0b1c0581cf6a7da24265f45
Samuel Christopher <Ebusameric@gmail.com>
2019-08-31 06:33:11 +0100

    features: add more styling and functionality

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -15,17 +15,19 @@ class Header extends React.Component {
 						<h1>Jakazautoparts</h1>
 						<span>Symbol of Quality</span>
 					</div>
-					<div className="address">
-						<div>Block G 75-76 Tundas International Market</div>
-						<div>Bakasi Ladipo (OSHODI, LAGOS)</div>
-					</div>
-					<div className="contact">
-						<div>Phone: +2349026586217</div>
-						<div>Whatsapp: +2348102578257</div>
-					</div>
-					<div className="time">
-						<div>Business Hours</div>
-						<div>Mon - Sat: 8:00am - 5:00pm</div>
+					<div className="container">
+						<div className="address">
+							<div>Block G 75-76 Tundas International <br/> Market Bakasi Ladipo,</div>
+							<div>(OSHODI, LAGOS)</div>
+						</div>
+						<div className="contact">
+							<div><span>Phone:</span> +2349026586217</div>
+							<div><span>Whatsapp:</span> +2348102578257</div>
+						</div>
+						<div className="time">
+							<div><span>Business Hours</span></div>
+							<div>Mon - Sat: 8:00am - 5:00pm</div>
+						</div>
 					</div>
 				</div>
 			</div>

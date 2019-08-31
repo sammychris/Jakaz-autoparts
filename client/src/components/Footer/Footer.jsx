48fa4206db93abced6adf5f21cd56b51de0f8dd1
Samuel Christopher <Ebusameric@gmail.com>
2019-08-31 19:09:30 +0100

    features: add more style on footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -13,30 +13,60 @@ class Footer extends React.Component {
 					<div className="footer-cont">
 						<div className="holder">
 							<div className="address">
-								<div>
-									Block G 75 - 76 Tundas International Market,
-									<br/>Bakasi Ladipo (Oshodi, Lagos).
+								<div className="icons">
+									<i className="fas fa-map-marker-alt"></i>
+									<div>
+										Block G 75 - 76 Tundas International Market,
+										<br/>Bakasi Ladipo (Oshodi, Lagos).
+									</div>
 								</div>
-								<div>
-									+2349026586217
+								<div className="icons">
+									<i className="fas fa-phone-square"></i>
+									<div>+2349026586217</div>
 								</div>
-								<div>
-									+2348102578257
+								<div className="icons">
+									<i className="fab fa-whatsapp-square"></i>
+									<div>+2348102578257</div>
 								</div>
 							</div>
 						</div>
 						<div className="holder">
 							<div className="foot-navigation">
-								<div>Search Inventory</div>
-								<div>Map & Direction</div>
-								<div>Contact Us</div>
+								<div className="icons">
+									<i className="fas fa-search-plus"></i>
+									<div>Search Inventory</div>
+								</div>
+								<div className="icons">
+									<i className="fas fa-map-marker"></i>
+									<div>Map & Direction</div>
+								</div>
+								<div className="icons">
+									<i className="fas fa-envelope"></i>
+									<div>Contact Us</div>
+								</div>
 							</div>
 						</div>
 						<div className="holder">
 							<div className="social">
 								<h3>Contact us</h3>
-								<div>Like us on Facebook</div>
-								<div>Gmail us</div>
+								<div className="icons">
+									<i className="fab fa-facebook-square"></i>
+									<div>Like us on Facebook</div>
+								</div>
+								<div>
+									<div className="icons">
+										<i className="fab fa-twitter-square"></i>
+										<div>Follow us on Twitter</div>
+									</div>
+									<div className="icons">
+										<i className="fab fa-google-plus-square"></i>
+										<div>Gmail Us</div>
+									</div>
+									<div className="icons">
+										<i className="fab fa-instagram"></i>
+										<div>Follow us on Instagram</div>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>

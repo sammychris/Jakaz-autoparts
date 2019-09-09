0c88c4aba122ba953fce391a98d1c8eab40763a0
samuel <ebusameric@gmail.com>
2019-09-09 21:01:26 +0100

    features: required needed files

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 
 require('dotenv').config();
 const express    = require('express');
-const mongoose   = require('mongoose');
-
+// get the client
+const mysql = require('mysql2');
+//const mongoose   = require('mongoose');
 const app = express();
+
+
+app.get('/', function (req, res) {
+	res.send('welcome sir')
+})
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DB, {useNewUrlParser: true});
+
+// mongoose.Promise = global.Promise;
+// mongoose.connect(process.env.DB, {useNewUrlParser: true});
 
 // http://expressjs.com/en/starter/static-files.html
 // app.use(express.static('public'));
@@ -18,14 +25,14 @@ mongoose.connect(process.env.DB, {useNewUrlParser: true});
 // import your route
 require('./route/api')(app);
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  // we're connected!
-  console.log('connected! to DB')
-});
+//var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   // we're connected!
+//   console.log('connected! to DB')
+// });
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log('Your spp is listening on port ' + listener.address().port);
 });

9ca934dfaa8507c024fe62cf7457f2b88dd11acc
samuel <ebusameric@gmail.com>
2019-09-10 21:04:02 +0100

    features: product routes

--- route/partRoutes.js
+++ route/partRoutes.js
@@ -3,25 +3,28 @@ require('dotenv').config();
 const multer  = require('multer');
 const path    = require('path');
 const Autoparts = require('../models/Autoparts');
+const Image = require('../models/AutopartImages');
 
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '../public/uploads'));
+    cb(null, path.join(__dirname, '../client/public/uploads'));
   },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + '-' + Date.now())
   }
 })
 
-const upload = multer({ storage });
 
+const upload = multer({ storage }).array('photos');
 
 module.exports = function (app) {
 
 app.route('/api/part')
-	.post((req, res) => {
+	.post(upload, (req, res) => {
 	  	const new_part = new Autoparts(req.body);
+	  	const { part_fk } = req.body;
+	  	const { files }  = req;
 
 	  	//handles null error 
 	  	for( let key in new_part ) {
@@ -29,17 +32,50 @@ app.route('/api/part')
 	  			return res.status(400).send({ error:true, message: 'Please provide '+key });
 	  		}
 	  	}
+	  	// handles null error for photo
+	  	if (!files.length || !part_fk) {
+	  		return res.status(400).send({ error:true, message: 'Please provide a photo'})
+	  	}
 		  
-	  	Autoparts.uploadNewPart(new_part, (err, result) => {
+	  	Autoparts.uploadNewPart(new_part, (err, output) => {
 		    if (err) return res.send(err);
-		    return res.json(result);
+		    req.files.forEach( each => {
+				const { filename } = each;
+				Image.uploadNewImages({ filename, part_fk }, (err, result) => {
+					if (err) return res.send(err);
+					return res.json(result);
+				});
+			});
 		  });
 	})
 
 	.get((req, res) => {
-		Autoparts.getAllParts((err, result) => {
-			return res.json(result);
-		})
+		const fetchProduct = new Promise((resolve, reject) => {
+			Autoparts.getAllParts((err, result) => {
+				resolve(result);
+			})
+		});
+
+		fetchProduct.then(a => res.json(a));
+
+		// async function fetchProduct() {
+		// 	const resii = await Autoparts.getAllParts((err, result) => {
+		// 		me =  await result;
+		// 	});
+
+		// 	console.log(me)
+
+		// 	return resii;
+		// 	// result.map(each => {
+		// 	// 		await Image.getByFK(each.id, (err, output) => {
+		// 	// 			each.photos = output;
+		// 	// 		});
+		// 	// 		return each;
+		// 	// 		//setTimeout(() => console.log(each), 4000)
+		// 	// 	})
+		// 	// 		//return res.json(new_res);
+		// }  
+		// fetchProduct().then(a => console.log(a));      
 	});
 
 

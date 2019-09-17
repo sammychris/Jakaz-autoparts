2f70626ca1e3ea74a4ea73c2bab334bab02c83e8
samuel <ebusameric@gmail.com>
2019-09-17 23:59:38 +0100

    add a production file path

--- server.js
+++ server.js
@@ -1,38 +1,30 @@
 
 require('dotenv').config();
-const express    = require('express');
-// get the client
+const express = require('express');
+const path = require('path');
 const mysql = require('mysql2');
-//const mongoose   = require('mongoose');
+
+
+
 const app = express();
 
 
-app.get('/', function (req, res) {
-	res.send('welcome sir')
-})
+// app.get('/', function (req, res) {
+// 	res.send('welcome sir')
+// })
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-
-
-// mongoose.Promise = global.Promise;
-// mongoose.connect(process.env.DB, {useNewUrlParser: true});
-
-// http://expressjs.com/en/starter/static-files.html
-// app.use(express.static('public'));
+// serving static files...
+if (process.env.NODE_ENV === 'production') {
+	console.log('production');
+	app.use('/static', express.static(path.join(__dirname, 'client/build')));
+} else console.log("Development!");
 
 // import your route
 require('./route/api')(app);
 
-//var db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function() {
-//   // we're connected!
-//   console.log('connected! to DB')
-// });
-
-// listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });

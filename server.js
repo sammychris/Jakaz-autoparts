2b81eac685b538cac561606d629832d657dd1746
samuel <ebusameric@gmail.com>
2019-09-18 23:56:49 +0100

    features: install newer package for url api fallback

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
 
 require('dotenv').config();
 const express = require('express');
-const path = require('path');
 const mysql = require('mysql2');
-
-
+const history = require('connect-history-api-fallback');
 
 const app = express();
-
-
 // app.get('/', function (req, res) {
 // 	res.send('welcome sir')
 // })
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(history());
+
 
 // serving static files...
 if (process.env.NODE_ENV === 'production') {
-	console.log('production');
-	app.use('/static', express.static(path.join(__dirname, 'client/build')));
-} else console.log("Development!");
+	console.log('Production environment!');
+	app.use(express.static('client/build'));
+} else { console.log('Development environment!')}
 
 // import your route
 require('./route/api')(app);

9d7ca7d05aee7ed679d8f59ba0ba6319a1048854
samuel <ebusameric@gmail.com>
2019-09-20 18:34:38 +0100

    features: add more functionality to product page

--- client/src/ProductPage/ProductPage.jsx
+++ client/src/ProductPage/ProductPage.jsx
@@ -1,22 +1,52 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
-import { Header, Navigation, Banner, About, Footer } from '../components';
+//import { Route, Link } from 'react-router-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 
 class ProductPage extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			product: {},
+			photos: [],
+		}
+		this.ProductPhotos = this.ProductPhotos.bind(this);
 	}
 
+	componentDidMount() {
+		const { params } = this.props.match;
+		fetch('/api/product/'+params.partid)
+			.then(res => res.json())
+			.then(res => {
+				const { product, photos } = res;
+				this.setState({ product, photos });
+			});
+	}
+
+	ProductPhotos(photos){
+	 	return photos.map(each => {
+			const { filename } = each;
+			return (
+				<div>
+                    <img src={'/uploads/'+filename}/>
+                    <p className="legend">Christopher 3</p>
+                </div>
+			)
+		})
+	 }
+
 	render() {
-		return (
+		const { product, photos } = this.state;
+		const { make, model, year, name  } = product;
+		return make ? (
 			<div className="container">
 				<div className="product">
-					<div className="main-img">
-						<img src="https://ae01.alicdn.com/kf/HTB1b6SeXoLrK1Rjy0Fjq6zYXFXaf/1-5-rc-baja-parts-Rovan-baja-rc-car-parts-BAJA-double-exhaust-pipe-with-muffler.jpg"/>
-					</div>
+					<Carousel infiniteLoop={true} autoPlay={true} className="main-img">
+		                { this.ProductPhotos(photos) }
+		            </Carousel>
 					<div className="write-up">
-						<h1>Product title</h1>
+						<h1>{`${make} ${model} ${year} ${name}`}</h1>
 						<div className="content">
 							<p>The main purpose</p>
 							<div>
@@ -26,7 +56,7 @@ class ProductPage extends React.Component {
 					</div>
 				</div>
 			</div>
-		);
+		): '';
 	}
 }
 

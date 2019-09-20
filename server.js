e87b48f9a88a804a12cb448e9d32bdf74cdafb4c
samuel <ebusameric@gmail.com>
2019-09-20 18:23:17 +0100

    features server static files

--- server.js
+++ server.js
@@ -2,26 +2,31 @@
 require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2');
-const history = require('connect-history-api-fallback');
+const path = require('path');
 
 const app = express();
-// app.get('/', function (req, res) {
-// 	res.send('welcome sir')
-// })
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(history());
 
 
 // serving static files...
-if (process.env.NODE_ENV === 'production') {
-	console.log('Production environment!');
-	app.use(express.static('client/build'));
-} else { console.log('Development environment!')}
+app.use(express.static('client/build/'));
+
 
 // import your route
 require('./route/api')(app);
+
+
+//serving all js data
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+});
+
 
 const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);

cfd8f12e65ddd382a0ef41040ca6fccce6b7b7f8
samuel <ebusameric@gmail.com>
2019-09-20 18:28:18 +0100

    features: add a category connection to db

--- models/Category.js
+++ models/Category.js
@@ -1,10 +1,7 @@
 const db = require('./db');
 
 //Task object constructor
-var Category = function(cat){
-    this.name = cat.name;
-    this.photo = cat.photo;
-};
+var Category = function(cat){};
 
 Category.createCategory = function (cat, result) {    
     db.query("INSERT INTO category set ?", cat, function (err, res) {    
@@ -20,7 +17,6 @@ Category.createCategory = function (cat, result) {
 };
 
 Category.getAll = function (result) {
-    //const sql = cat ? "select * from `products` where `name` = ? ": "SELECT * FROM products";
     db.query("SELECT * FROM category", function (err, res) {
         if(err) {
             console.log("error: ", err);
@@ -33,41 +29,18 @@ Category.getAll = function (result) {
     });   
 };
 
-Category.getById = function (catId, result) {
-    db.query("Select * from category where id = ? ", catId, function (err, res) {             
+Category.getProductByCat = function (cat_id, result) {
+    db.query("select * from `products` where `category_id` = ? ", cat_id, function (err, res) {
         if(err) {
             console.log("error: ", err);
-            result(err, null);
+            result(null, err);
         }
         else{
+            //console.log('product : ', res);  
             result(null, res);
         }
     });   
 };
 
-Category.updateById = function(id, cat, result){
-    db.query("UPDATE category SET ? WHERE id = ?", [cat, id], function (err, res) {
-        if(err) {
-            console.log("error: ", err);
-            result(null, err);
-        }
-        else{
-            result(null, res);
-        }
-    }); 
-};
-
-Category.remove = function(id, result){
-    db.query("DELETE FROM category WHERE id = ?", [id], function (err, res) {
-
-        if(err) {
-            console.log("error: ", err);
-            result(null, err);
-        }
-        else{
-         result(null, res);
-        }
-    }); 
-};
 
 module.exports = Category;

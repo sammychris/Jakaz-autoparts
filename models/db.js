b7b0e5e41cc3ba9530576fab0e4a1c51598a1b7a
samuel <ebusameric@gmail.com>
2019-09-22 13:59:12 +0100

    features: add another db connection

--- models/db.js
+++ models/db.js
@@ -3,18 +3,21 @@ const mysql = require('mysql2');
 
 
 // create the connection to database
-const connection = mysql.createConnection({
-  host: 'sql2.freemysqlhosting.net',
-  user: 'sql2305177',
-  password: 'sP4%pX6!',
+const connection = mysql.createPool({
+  host: 'sql7.freemysqlhosting.net',
+  user: 'sql7307084',
+  password: 'zsPFMcrDr7',
   port: '3306',
-  database: 'sql2305177'
+  database: 'sql7307084',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 
-connection.connect(function(err) {
-  if (err) return console.error(err);
-  console.log("Connected!");
-});
+// connection.connect(function(err) {
+//   if (err) return console.error(err);
+//   console.log("Connected!");
+// });
 
 module.exports = connection;

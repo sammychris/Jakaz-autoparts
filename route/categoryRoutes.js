f68eadab9e3c55397e9e616124a3e198fa70758e
samuel <ebusameric@gmail.com>
2019-09-24 14:10:19 +0100

    features: add a new file uploads in server

--- route/categoryRoutes.js
+++ route/categoryRoutes.js
@@ -7,7 +7,7 @@ const Category = require('../models/Category');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '../client/public/uploads'));
+    cb(null, path.join(__dirname, '../uploads'));
   },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + '-' + Date.now())

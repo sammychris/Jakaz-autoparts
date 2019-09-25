81a989e584e69aa416ee564f1dd81ec233fb3c3e
samuel <ebusameric@gmail.com>
2019-09-25 14:26:26 +0100

    features: complete the banner and styling

--- client/src/components/Banner/Banner.jsx
+++ client/src/components/Banner/Banner.jsx
@@ -1,79 +1,39 @@
 import React from 'react';
-// import AliceCarousel from 'react-alice-carousel';
-// import "react-alice-carousel/lib/alice-carousel.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
+import Search from '../Search';
 
 
-const search = {
-	display: 'block',
-	height: '100vh',
-	zIndex: '1',
-}
-
-class Banner extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-
-	render() {
-		// return (
-		// 	<div className="hero">
-		// 		<div className="top-call">
-		// 			<a href="#">
-		// 				<button id="srchBtn">
-		// 					SEARCH OUR INVENTORY
-		// 				</button>
-		// 			</a>
-		// 			<div className="wrnty">
-		// 				<div id="day">60 day</div>
-		// 				<div id="warranty">WARRANTY</div>
-		// 			</div>
-		// 		</div>
-		// 		<div className="mid-call">
-		// 			<h1>In Need of Any Car Parts? Please Call Us to Get Prices!</h1>
-		// 		</div>
-		// 	</div>
-		// )
-
-		return (
-			<div className="banner">
-
-				{
-					// <div className="hero">
-					// 				<div className="top-call">
-					// 					<a href="#">
-					// 						<button id="srchBtn">
-					// 							SEARCH OUR INVENTORY
-					// 						</button>
-					// 					</a>
-					// 					<div className="wrnty">
-					// 						<div id="day">60 day</div>
-					// 						<div id="warranty">WARRANTY</div>
-					// 					</div>
-					// 				</div>
-					// 				<div className="mid-call">
-					// 					<h1>In Need of Any Car Parts? Please Call Us to Get Prices!</h1>
-					// 				</div>
-					// 			</div>
-							}
-				<div className="outside">
-					<div className="inside">orange</div>
+function Banner () {
+	return (
+		<div className="banner">
+			<div className="hero">
+				<div className="float-search">
+					<Search />
 				</div>
-	            <Carousel infiniteLoop={true} autoPlay={true} showThumbs={false} >
-	                <div className="banner-slide">
-	                    <img src="/images/banner2.jpg" />
-	                    <p className="legend">In Need of Any Car Parts? Please Call Us to Get Prices!</p>
-	                </div>
-	                <div className="banner-slide">
-	                    <img src="/images/banner3.jpg" />
-	                    <p className="legend mid-call">engines</p>
-	                </div>
-	            </Carousel>
-            </div>
-        );
-	}
-
+			</div>
+			<div className="hero">
+				<div className="wrnty">
+					<div id="day">60 day</div>
+					<div id="warranty">WARRANTY</div>
+				</div>
+			</div>
+            <Carousel infiniteLoop={true} autoPlay={true} showThumbs={false}>
+            	<div className="banner-slide">
+                    <img src="/images/banner.jpg" />
+                    <p className="legend">In Need of Any Car Parts? Please Call Us to Get Prices!</p>
+                </div>
+                <div className="banner-slide">
+                    <img src="/images/banner1.jpg" />
+                    <p className="legend">We also deliver to your door step</p>
+                </div>
+                <div className="banner-slide">
+                    <img src="/images/banner2.jpg" />
+                    <p className="legend">We also deliver to your door step</p>
+                </div>
+            </Carousel>
+        </div>
+    );
 }
 
 export default Banner;

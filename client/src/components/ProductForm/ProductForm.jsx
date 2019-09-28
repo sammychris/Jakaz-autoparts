ef428b1079c267e4991e61487084e76ddd0a46b6
samuel <ebusameric@gmail.com>
2019-09-28 14:56:35 +0100

    features: add a functionality

--- client/src/components/ProductForm/ProductForm.jsx
+++ client/src/components/ProductForm/ProductForm.jsx
@@ -90,12 +90,12 @@ class ProductForm extends React.Component {
 			<div className="prod" style={productform}>
 				<h2>Add a product</h2>
 				<form onSubmit={this.handleSubmit} encType="multipart/form-data">
-					<div>Make: <input type="text" value={make} name="make" onChange={this.handleChange}/></div> <br/>
-					<div>Model: <input type="text" value={model} name="model" onChange={this.handleChange}/></div> <br/>
+					<div>Make: <input type="text" required value={make} name="make" onChange={this.handleChange}/></div> <br/>
+					<div>Model: <input type="text" required value={model} name="model" onChange={this.handleChange}/></div> <br/>
 					<div>Year: <input type="text" value={year} name="year" onChange={this.handleChange}/></div> <br/>
-					<div>Part Name: <input type="text" value={name} name="name" onChange={this.handleChange}/></div> <br/>
-					<div>Category: <select value={catName} name="catName" onChange={this.handleChange}>
-							<option>select a Category</option>
+					<div>Part Name: <input type="text" required value={name} name="name" onChange={this.handleChange}/></div> <br/>
+					<div>Category: <select value={catName} required name="catName" onChange={this.handleChange}>
+							<option value="">select a Category</option>
 							{ this.listCategory(categories) }
 						</select>
 					</div><br/>

19f609fd4d4e48701d888aa83cd92945bb5da511
samuel <ebusameric@gmail.com>
2019-10-01 17:56:25 +0100

    clean all codes

--- client/src/HomePage/HomePage.jsx
+++ client/src/HomePage/HomePage.jsx
@@ -1,23 +1,17 @@
 import React from 'react';
-import { Header, Navigation, Banner, About, Footer } from '../components';
+import { Banner, About } from '../components';
 
 
-class HomePage extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-
-	render() {
-		return (
-			<div className="container">
-				<Banner />
-				<About />
-				<div id="btm-call">
-					We are proud of our works.
-				</div>
+function HomePage() { 
+	return (
+		<div className="container">
+			<Banner />
+			<About />
+			<div id="btm-call">
+				We are proud of our works.
 			</div>
-		)
-	}
+		</div>
+	)
 }
 
 export default HomePage;

a77b24242e02195bf2a0b37d0783b4a34033d806
samuel <ebusameric@gmail.com>
2019-10-01 17:57:36 +0100

    clean add codes

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -2,42 +2,35 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-class Header extends React.Component {
-	constructor(props) {
-		super(props)
-	}
-
-	render() {
-		return (
-			<div className="header">
-				<div className="header-content">
-					<Link to="/" className="logo" style={{ display: 'flex' }}>
-						<div style={{ paddingRight: '20px'}}>
-							<img src="/images/logo.png"/>
-						</div>
-						<div>
-							<img src="/images/brand.png"/>
-						</div>
-					</Link>
-					<div className="container">
-						<div className="address">
-							<div>Block G 75-76 Tundas<br/>International Market Bakasi</div>
-							<div>Ladipo (OSHODI, LAGOS)</div>
-						</div>
-						<div className="contact">
-							<div><span>Phone:</span> +2349026586217</div>
-							<div><span>Whatsapp:</span> +2348102578257</div>
-						</div>
-						<div className="time">
-							<div><span>Business Hours</span></div>
-							<div>Mon - Sat: 8:00am - 5:00pm</div>
-						</div>
+function Header() {
+	return (
+		<div className="header">
+			<div className="header-content">
+				<Link to="/" className="logo" style={{ display: 'flex' }}>
+					<div style={{ paddingRight: '20px'}}>
+						<img alt="" src="/images/logo.png"/>
+					</div>
+					<div>
+						<img alt="" src="/images/brand.png"/>
+					</div>
+				</Link>
+				<div className="container">
+					<div className="address">
+						<div>Block G 75-76 Tundas<br/>International Market Bakasi</div>
+						<div>Ladipo (OSHODI, LAGOS)</div>
+					</div>
+					<div className="contact">
+						<div><span>Phone:</span> +2349026586217</div>
+						<div><span>Whatsapp:</span> +2348102578257</div>
+					</div>
+					<div className="time">
+						<div><span>Business Hours</span></div>
+						<div>Mon - Sat: 8:00am - 5:00pm</div>
 					</div>
 				</div>
 			</div>
-		)
-	}
-
+		</div>
+	)
 }
 
 export default Header;

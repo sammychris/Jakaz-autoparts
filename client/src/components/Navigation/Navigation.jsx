c725177215a87ea23a38e0087ac7e84df325570f
samuel <ebusameric@gmail.com>
2019-10-02 18:07:36 +0100

    change contructor to function

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -4,38 +4,31 @@ import ResponsiveMenu from 'react-responsive-navbar';
 
 
 
-class Navigation extends React.Component {
-	constructor(props) {
-		super(props)
-	}
-
-	render() {
-		return (
-			<div className="navigation">
-				<ResponsiveMenu
-			        menuOpenButton={<i className="fas fa-bars"></i>}
-			        menuCloseButton={<i className="fas fa-times"></i>}
-			        changeMenuOn="500px"
-			        largeMenuClassName="large-menu-classname"
-			        smallMenuClassName="small-menu-classname"
-			        menu={
-			          	<ul>
-							<Link to="/parts">
-								<li>Car Parts</li>
-							</Link>
-							<Link to="/map&direction">
-								<li>Map & Direction</li>
-							</Link>
-							<Link>
-								<li>Contact Us</li>
-							</Link>
-						</ul>
-			        }
-			    />
-			</div>
-		)
-	}
-
+function Navigation() {
+	return (
+		<div className="navigation">
+			<ResponsiveMenu
+		        menuOpenButton={<i className="fas fa-bars"></i>}
+		        menuCloseButton={<i className="fas fa-times"></i>}
+		        changeMenuOn="500px"
+		        largeMenuClassName="large-menu-classname"
+		        smallMenuClassName="small-menu-classname"
+		        menu={
+		          	<ul>
+						<Link to="/parts">
+							<li>Car Parts</li>
+						</Link>
+						<Link to="/map&direction">
+							<li>Map & Direction</li>
+						</Link>
+						<Link to="/contact">
+							<li>Contact Us</li>
+						</Link>
+					</ul>
+		        }
+		    />
+		</div>
+	)
 }
 
 export default Navigation;

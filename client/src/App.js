2a26b34b210f8017694617787fb09b65c7db5110
samuel <ebusameric@gmail.com>
2019-10-08 20:22:03 +0100

    features: display first loading

--- client/src/App.js
+++ client/src/App.js
@@ -7,29 +7,45 @@ import { ProductPage } from './ProductPage';
 import { Notfound } from './Notfound';
 import { AdminPage } from './AdminPage';
 import { MapDirection } from './MapDirection';
+import { ContactPage } from './ContactPage';
 import { SearchPage } from './SearchPage';
 
-import { Header, Navigation, Footer } from './components';
+import { Header, Navigation, Footer, FirstLoading } from './components';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false,
+    }
+  }
+  
+  componentDidMount(){
+    this.setState({ isReady: true });
+  }
+
   render() {
     return (
       <Router>
-        <div className="container">
-          <Header />
-          <Navigation />
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route exact path="/parts" component={ListProductPage} />
-            <Route exact path="/parts/search" component={SearchPage} />
-            <Route exact path="/parts/:pname" component={ListProductPage} />
-            <Route path="/parts/:pname/:partid" component={ProductPage} />
-            <Route path="/productmanager" component={AdminPage} />
-            <Route path="/Map&direction" component={MapDirection} />
-            <Route component={Notfound} />
-          </Switch>
-          <Footer />
-        </div>
+        { this.state.isReady
+          ? <div className="container">
+              <Header />
+              <Navigation />
+              <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route exact path="/parts" component={ListProductPage} />
+                <Route exact path="/parts/search" component={SearchPage} />
+                <Route exact path="/parts/:pname" component={ListProductPage} />
+                <Route path="/parts/:pname/:partid" component={ProductPage} />
+                <Route path="/productmanager" component={AdminPage} />
+                <Route path="/contact" component={ContactPage} />
+                <Route path="/map&direction" component={MapDirection} />
+                <Route component={Notfound} />
+              </Switch>
+              <Footer />
+            </div>
+          : <FirstLoading />
+        }
       </Router>
     )
   }
